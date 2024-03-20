e255bf6e58f85b50f471b242b04f129d4d38fe1a
Mahdi Bey <mahdibeyy@gmail.com>
2024-03-21 00:21:55 +0100

    thir commit

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.use(bodyParser.json());
 
 app.use('/submitForm', submitFormRoute);
 // Servir les fichiers statiques de l'application Angular
-app.use(express.static(path.join(__dirname, 'D_Documents/Angular/CNC_Project/cncs/dist/cncs')));
+app.use(express.static(path.join(__dirname, 'D_Documents/Angular/CNC_Project/cncs')));
 
 // Rediriger toutes les autres routes vers l'index.html de l'application Angular
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'D_Documents/Angular/CNC_Project/cncs/dist/cncs/index.html'));
+    res.sendFile(path.join(__dirname, 'D_Documents/Angular/CNC_Project/cncs/src/index.html'));
 });
 
 
