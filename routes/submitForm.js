bede496ba8e8f86848606c7753d79f9b8e01f7bc
Mahdi Bey <mahdibeyy@gmail.com>
2024-03-20 03:27:37 +0100

    thir commit

--- routes/submitForm.js
+++ routes/submitForm.js
@@ -12,6 +12,9 @@ const transporter = nodemailer.createTransport({
 });
 
 router.post('/ajout', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Permettre à tous les domaines d'accéder à l'API (à ajuster en fonction de vos besoins de sécurité)
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Méthodes HTTP autorisées
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // En-têtes autorisés
     const { nomSociete, telephone, email, adresseSociete, typeMachine, refMachine, descriptionPanne } = req.body;
 
     try {
--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const submitFormRoute = require('./routes/submitForm.js');
 
 const app = express();
 
-// Middleware pour gérer CORS
+app.use(cors()); // Autoriser toutes les requêtes CORS
+// Ajouter middleware pour gérer CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Permettre à tous les domaines d'accéder à l'API (à ajuster en fonction de vos besoins de sécurité)
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Méthodes HTTP autorisées
