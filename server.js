759c02986219424dc61ce19736fe40129ec381bc
Mahdi Bey <mahdibeyy@gmail.com>
2024-03-20 02:58:17 +0100

    thir commit

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ const submitFormRoute = require('./routes/submitForm.js');
 
 const app = express();
 
-app.use(cors()); // Autoriser toutes les requêtes CORS
+// Middleware pour gérer CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Permettre à tous les domaines d'accéder à l'API (à ajuster en fonction de vos besoins de sécurité)
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Méthodes HTTP autorisées
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // En-têtes autorisés
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
