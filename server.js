445c6c6ab89c993fc554127f4ba59a44924a9ed9
Mahdi Bey <mahdibeyy@gmail.com>
2024-03-20 14:24:41 +0100

    thir commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path');
+
 const mongoose = require('./config/connect.js');
 
 const submitFormRoute = require('./routes/submitForm.js');
@@ -23,9 +23,6 @@ app.use(bodyParser.json());
 
 app.use('/submitForm', submitFormRoute);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'D:\\D_Documents\\Angular\\CNC_Project\\cncs\\src\\index.html'));
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
