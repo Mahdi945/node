6d6435b08cd4258381bd59b58494ee7dec5407d5
Mahdi Bey <mahdibeyy@gmail.com>
2024-03-20 02:01:31 +0100

    thir commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path'); // Module path pour travailler avec les chemins de fichiers
 const mongoose = require('./config/connect.js'); // Importez votre connexion à MongoDB
 
 const submitFormRoute = require('./routes/submitForm.js');
@@ -14,6 +15,11 @@ app.use(bodyParser.json());
 
 // Utilisation de la route pour gérer les formulaires
 app.use('/submitForm', submitFormRoute);
+
+// Redirection pour servir l'application front-end Angular
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'D:\\D_Documents\\Angular\\CNC_Project\\cncs\\src\\index.html')); // Remplacez 'path_vers_votre_index.html' par le chemin vers votre fichier index.html
+});
 
 const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
