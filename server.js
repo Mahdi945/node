7dbaf500bba505aef06ff8845df46a37e9589d35
Mahdi Bey <mahdibeyy@gmail.com>
2024-03-26 20:22:39 +0100

    thir commit

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const mongoose = require('./config/connect.js');
+
 
 const submitFormRoute = require('./routes/submitForm.js');
 
 const app = express();
 
-app.use(cors()); // Autoriser toutes les requêtes CORS
-// Ajouter middleware pour gérer CORS
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Permettre à tous les domaines d'accéder à l'API (à ajuster en fonction de vos besoins de sécurité)
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Méthodes HTTP autorisées
